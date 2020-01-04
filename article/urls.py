4799682d22e8fcb803af52d943e7a19178b15290
sehrigokcan <sehrigokcan@gmail.com>
2020-01-04 13:04:32 +0100

    sql

--- article/urls.py
+++ article/urls.py
@@ -7,16 +7,13 @@ app_name = 'article'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-
     path('creat/', views.creat, name='creat'),
     path('creat/addarticle', views.addArticle, name='addarticle'),
-
     path('articles/', views.articles, name='articles'),
     path('articles/article/<int:id>', views.detail, name='detail'),
     path('articles/update/<int:id>', views.updateArticle, name='update'),
     path('articles/delete/<int:id>', views.deleteArticle, name='delete'),
     path('articles/comment/<int:id>', views.addComment, name='comment'),
-
     path('article/reading/<int:id>', views.reading, name="reading"),
 
     path('like/', views.like_post, name="like_article"),
